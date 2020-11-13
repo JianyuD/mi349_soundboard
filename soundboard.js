66ab16470e9a9f4deb281ac5d697b30293d76da2
JianyuD <dengjia3@msu.edu>
2020-11-13 17:48:37 +0800

    update

---
+++ soundboard.js
@@ -0,0 +1,11 @@
+function playdoorbell() {
+    document.getElementById("doorbell").play();
+}
+
+function playexplosion() {
+    document.getElementById("explosion").play();
+}
+
+function playgolf() {
+    document.getElementById("golf").play();
+}
